549105eb76bc2da607cc2daa90821e2ff884a031
KIM JAE YEONG <110236609+rlawodud89@users.noreply.github.com>
2026-07-24 22:33:59 +0900

    Common 코드 이식

---
+++ block-racing-common/block-racing-common/Network/Packets/S_StartGamePacket.cs
@@ -0,0 +1,23 @@
+﻿
+namespace block_racing_common.Network.Packets
+{
+    public class S_StartGamePacket : IPacket
+    {
+        public PacketId PacketId => PacketId.S_StartGame;
+
+        public int RoomId { get; set; }
+        public float CountdownSeconds { get; set; } = 3f;
+
+        public void Read(PacketReader reader)
+        {
+            RoomId = reader.ReadInt32();
+            CountdownSeconds = reader.ReadFloat();
+        }
+
+        public void Write(PacketWriter writer)
+        {
+            writer.Write(RoomId);
+            writer.Write(CountdownSeconds);
+        }
+    }
+}
