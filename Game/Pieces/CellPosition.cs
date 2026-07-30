a4e88255d751f6fe1ff31cca6ecc93f1d66709a7
KIM JAE YEONG <110236609+rlawodud89@users.noreply.github.com>
2026-07-30 14:01:44 +0900

    feat: 블록 모양 정의 클래스(PieceShapeTable) Common으로 이동

---
+++ Game/Pieces/CellPosition.cs
@@ -0,0 +1,14 @@
+﻿
+namespace block_racing_common.Game.Pieces;
+
+public readonly struct CellPosition
+{
+    public readonly int X;
+    public readonly int Y;
+
+    public CellPosition(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
